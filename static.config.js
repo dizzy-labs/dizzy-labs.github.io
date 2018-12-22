527856f55bfb1a2c4fde40393aea07f52222d167
dizzy <42190485+dizzy-labs@users.noreply.github.com>
2018-12-22 03:43:20 -0600

    removed leftover code that depended on a removed container

--- static.config.js
+++ static.config.js
@@ -4,26 +4,6 @@ export default {
   getSiteData: () => ({
     title: 'Dizzy Directory'
   }),
-  getRoutes: async () => {
-    const { data: posts } = await axios.get(
-      'https://jsonplaceholder.typicode.com/posts'
-    )
-    return [
-      {
-        path: '/blog',
-        getData: () => ({
-          posts,
-        }),
-        children: posts.map(post => ({
-          path: `/post/${post.id}`,
-          component: 'src/containers/Post',
-          getData: () => ({
-            post,
-          }),
-        })),
-      },
-    ]
-  },
   Document: ({ Html, Head, Body, children, siteData, renderMeta }) => (
     <Html lang="en-US">
       <Head>
