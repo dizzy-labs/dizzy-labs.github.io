b51a33b497e05998aa68c4838e41d84b0dedf8ce
Dizzy <42190485+dizzy-labs@users.noreply.github.com>
2019-02-12 22:31:31 -0600

    Updates

--- react-static-templates.js
+++ react-static-templates.js
@@ -1,25 +1,11 @@
 
 
-import universal, { setHasBabelPlugin } from '/home/dizzy/Code/dizzy-labs.github.io/node_modules/react-universal-component/dist/index.js'
-  
 
-
-setHasBabelPlugin()
-
-const universalOptions = {
-  loading: () => null,
-  error: props => {
-    console.error(props.error);
-    return <div>An error occurred loading this page's template. More information is available in the console.</div>;
-  },
-}
-
-const t_0 = universal(import('../src/pages/404.js'), universalOptions)
-const t_1 = universal(import('../src/pages/emojos.js'), universalOptions)
-const t_2 = universal(import('../src/pages/index.js'), universalOptions)
-const t_3 = universal(import('../src/pages/pico-8.js'), universalOptions)
-const t_4 = universal(import('../src/containers/pico8-game'), universalOptions)
-
+import t_0 from '../src/pages/404.js'
+import t_1 from '../src/pages/emojos.js'
+import t_2 from '../src/pages/index.js'
+import t_3 from '../src/pages/pico-8.js'
+import t_4 from '../src/containers/pico8-game'
 
 export const template404ID = 0
 
--- templates/styles.94bc5369.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{103:function(n,o,w){},105:function(n,o,w){},109:function(n,o,w){}}]);
-//# sourceMappingURL=styles.94bc5369.js.map
