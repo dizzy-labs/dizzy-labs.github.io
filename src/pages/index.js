d9446731fbb5e61f86930025c4138f02ca1a2d23
Dizzy <42190485+dizzy-labs@users.noreply.github.com>
2019-02-12 23:11:33 -0600

    linting

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { withSiteData } from 'react-static'
-//import { Link } from '@reach/router'
+// import { Link } from '@reach/router'
 
 export default withSiteData(() => (
   <div style={{ textAlign: 'center' }}>
